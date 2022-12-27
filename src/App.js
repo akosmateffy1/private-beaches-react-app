6a6a5e78c9ce8b99bc348c91ea525cf2eb86091b
Ákos Mátéffy <102735443+akosmateffy1@users.noreply.github.com>
2022-12-28 00:26:38 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import ImageSlider from './components/ImageSlider';
+import Navbar from './components/Navbar';
+import Offers from './components/Offers';
+import Plan from './components/Plan';
+import Rooms from './components/Rooms';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Offers />
+      <Plan />
+      <Rooms />
+      <ImageSlider />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
